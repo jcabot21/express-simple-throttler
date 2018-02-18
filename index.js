fddbe1db2ccd1cb10cb1f3c8c519acebfc42bb1a
Jorge Cabot <jcabot21@gmail.com>
2018-02-17 19:02:11 -0500

    Added middleware code.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Promise = require('bluebird');
 const random = require('lodash.random');
 
-module.exports = () => {
-
-};
+module.exports = (delay, randomize = false) => 
+    (request, response, next) => 
+        Promise.delay(!randomize ? delay : random(0, delay))
+            .then(() => next());
